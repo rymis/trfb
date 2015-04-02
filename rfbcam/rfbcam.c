04663fed4b573cd879b306293be16fc7b3211e11
Mikhail Ryzhov <rymiser@gmail.com>
2015-04-02 18:49:09 +0300

    Using libwebcam

--- rfbcam/rfbcam.c
+++ rfbcam/rfbcam.c
@@ -1,12 +1,9 @@
 #include <trfb.h>
 #include <signal.h>
 #include <string.h>
-
-#ifdef USE_LWEBCAM
-# include "lwebcam/lwebcam.h"
-#else
-# include "webcam/webcam.h"
-#endif
+#include <libwebcam.h>
+#include <stdio.h>
+#include <unistd.h>
 
 static int quit_now = 0;
 static void sigint(int sig)
@@ -14,21 +11,12 @@ static void sigint(int sig)
 	quit_now = 1;
 }
 
-#ifdef USE_LWEBCAM
-static void draw_image(struct webcam *cam, trfb_server_t *srv)
-#else
-static void draw_image(webcam_t *cam, buffer_t *buf, trfb_server_t *srv)
-#endif
+static void draw_image(webcam_t *cam, trfb_server_t *srv)
 {
 	unsigned x, y;
 	unsigned W = cam->width * 3;
 	unsigned off;
-	unsigned char *data =
-#ifdef USE_LWEBCAM
-		cam->img_data;
-#else
-		buf->start;
-#endif
+	unsigned char *data = cam->img;
 
 	trfb_server_lock_fb(srv, 1);
 
@@ -50,25 +38,14 @@ int main(int argc, char *argv[])
 	const unsigned width = 640;
 	const unsigned height = 480;
 	trfb_event_t event;
-#if !defined(USE_LWEBCAM)
-	buffer_t frame = { NULL, 0 };
-#endif
 
 	signal(SIGINT, sigint);
 
-#ifdef USE_LWEBCAM
-	cam = webcam_open("/dev/video0", WEBCAM_IO_METHOD_READ, width, height);
-#else
-	cam = webcam_open("/dev/video0");
-#endif
+	cam = webcam_open(0, 640, 480);
 	if (!cam) {
 		fprintf(stderr, "Error: can't open webcam!\n");
 		return 1;
 	}
-
-#if !defined(USE_LWEBCAM)
-	webcam_resize(cam, width, height);
-#endif
 
 	srv = trfb_server_create(width, height, 4);
 	if (!srv) {
@@ -86,21 +63,12 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-#ifdef USE_LWEBCAM
-	webcam_start_capturing(cam);
-#else
-	webcam_stream(cam, 1);
-#endif
+	webcam_start(cam);
 	for (;;) {
 		if (trfb_server_updated(srv)) {
-#ifdef USE_LWEBCAM
-			if (webcam_wait_frame(cam, 1) > 0) {
+			if (webcam_wait_frame(cam, 10) > 0) {
 				draw_image(cam, srv);
 			}
-#else
-			webcam_grab(cam, &frame);
-			draw_image(cam, &frame, srv);
-#endif
 		}
 
 		while (trfb_server_poll_event(srv, &event)) {
@@ -123,11 +91,7 @@ int main(int argc, char *argv[])
 		usleep(1000);
 		/* TODO: wait for update */
 	}
-#ifdef USE_LWEBCAM
-	webcam_stop_capturing(cam);
-#else
-	webcam_stream(cam, 0);
-#endif
+	webcam_stop(cam);
 	webcam_close(cam);
 	trfb_server_stop(srv);
 	trfb_server_destroy(srv);
